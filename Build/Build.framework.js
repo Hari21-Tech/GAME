5800f95019b22e2cb79085d6a2dc654af62bcb5d
Aargh Rai <shubh2007rai@gmail.com>
2025-08-02 00:32:01 +0530

    hopefull fix

--- Build/Build.framework.js
+++ Build/Build.framework.js
@@ -1294,10 +1294,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  6148864: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 6148919: ($0) => { performance.now = function() { return $0; }; },  
- 6148967: ($0) => { performance.now = function() { return $0; }; },  
- 6149015: () => { performance.now = Module['emscripten_get_now_backup']; }
+  6148688: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 6148743: ($0) => { performance.now = function() { return $0; }; },  
+ 6148791: ($0) => { performance.now = function() { return $0; }; },  
+ 6148839: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
